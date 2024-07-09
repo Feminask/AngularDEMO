7c233746c0734d7240e5a316db39b9c9e919bc88
Feminask <fmnzrasheed@gmail.com>
2024-07-10 02:19:07 +0530

    event binding with template rendering variable

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,35 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  data: string = "welcome to login page";
-  secondData: string = "Enter the username";
-uname:any=""
-psw:any=""
+  data: string = 'welcome to login page';
+  secondData: string = 'Enter the username';
+  uname: any = '';
+  psw: any = '';
   constructor() {}
-
-  ngOnInit(): void {
-  
+  ngOnInit(): void {}
+  login(a:any,b:any) {
+    this.uname=a.value
+    this.psw=b.value
+    alert(`${this.uname} & ${this.psw}`);
   }
-
-  login(){
-    alert('login clicked')
-  }
-
-accessUname(event:any){
-this.uname=event.target.value
-console.log(this.uname);
-
-
-}
-
-accessPSW(event:any){
-  this.psw=event.target.value
-  console.log(this.psw);
-  
-
-}
-
+//   accessUname(event: any) {
+//     this.uname = event.target.value;
+//     console.log(this.uname);
+//   }
+//   accessPSW(event: any) {
+//     this.psw = event.target.value;
+//     console.log(this.psw);
+//   }
 }
